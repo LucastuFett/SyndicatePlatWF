66ca1c988c63ab29e7a2cc2b3b35ed32c83f339e
LucastuFett <lucasgutie@hotmail.com>
2024-02-26 02:54:11 -0300

    Red Veil Added

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# warframe.market-api-example
-An example on how to do authorized requests with the warframe.market API.
+# SyndicatePlatWF
 
-## Why?
-I was unfamiliar with this and it took me ages to figure out. The documentation didn't really help and I couldn't find any good examples. If you're here you're probably in the same spot I was in. Have fun coding.
-
-## Getting your token
-1. Use your favorite browser (e.g. Firefox or Chrome)
-2. Log into warframe.market (if you aren't already)
-3. Pull up the developer console (ctrl + shift + i)
-4. Navigate to the tab "Storage"
-5. Click on the "Cookies" storage
-6. Go the cookies for the warframe.market domain
-7. Select the cookie called "JWT" (that stands for JSON Web Token)
-8. Copy the value of the JWT cookie
-9. Paste the value into token.json (needs to be a string)
-
-## Documentation
-Official API documentation (because it's not easy to find):
-* [Link to the full v1 documentation](https://docs.google.com/document/d/1121cjBNN4BeZdMBGil6Qbuqse-sWpEXPpitQH5fb_Fo/)
-* [Link to a document](https://docs.google.com/document/d/13bbbbhe9nY4XvUG8yzUj-GaSVkQIqg1bPS5jpFg_wI0/) - This is just a document with a link to the first document, no need to click this. *Maybe this is where v2 docs will be in the future.*
-
-## Support
-I'm not gonna help you with this, you're on your own. The warframe.market Discord has a support channel though.
+Syndicate Platinum Analysis for Warframe
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 //Dependencies
 const axios = require("axios").default	//The HTTP request client
 
-//Info
-const token = require("./token.json")
-
 //Create a client instance
 const market = axios.create({
 	baseURL: "https://api.warframe.market/v1",
-	timeout: 1000,
+	timeout: 2000,
 	headers: {
 		"content-type": "application/json",
 		"accept": "application/json",
@@ -16,39 +13,65 @@ const market = axios.create({
 	}
 })
 
-const nkn1396 = axios.create({
-	baseURL: "https://api.warframe.market/v1",
-	timeout: 1000,
-	headers: {
-		"content-type": "application/json",
-		"accept": "application/json",
-		"platform": "pc",
-		"language": "en",
-		"authorization": `JWT ${token}`,
-	}
-})
+//Parse all items in red veil syndicate
+const items = require("./red_veil.json")
+var parsedItems = []
+for (let i = 0; i < items.length; i++) {
+	setTimeout(() => {
+		var item = items[i]
+		market.get(`/items/${item.id}/orders`)
+			.then(response => {
+				var prices = []
+				for (const order of response.data.payload.orders) {
+					if(order.order_type === "sell" && order.user.region === "en" && order.user.reputation > 0){
+						prices.push(order.platinum)
+					}
+				}
+				const min = Math.min(...prices)
+				const max = Math.max(...prices)
+				const avg = (prices.reduce((a, b) => a + b, 0) / prices.length).toFixed(2)
+				const med = prices.sort((a, b) => a - b)[Math.floor(prices.length / 2)]
+				const minPlatPer10kRep = min / (item.price / 10000)
+				const avgPlatPer10kRep = (avg / (item.price / 10000)).toFixed(2)
+				const medianPlatPer10kRep = med / (item.price / 10000)
+				const maxPlatPer10kRep = max / (item.price / 10000)
 
-/*
-//Anonymous request
-market.get("/items")
-	.then(response => {
-		const fs = require("fs")
-		fs.writeFile("./items.json", JSON.stringify(response.data), {}, ()=>{})
-		console.log(response.data)
-	})
-	.catch(console.error)
-//*/
+				const parsedData = {
+					title: item.title,
+					minimum: min,
+					average: avg,
+					median: med,
+					maximum: max,
+					minPlatPer10kRep: minPlatPer10kRep,
+					averagePlatPer10kRep: avgPlatPer10kRep,
+					medianPlatPer10kRep: medianPlatPer10kRep,
+					maxPlatPer10kRep: maxPlatPer10kRep
+				};
+				parsedItems.push(parsedData)
 
-//Authorized request
-//The following puts one Latron Prime Receiver on warframe.market
-nkn1396.post("/profile/orders", {
-	"order_type": "sell",
-	"item_id": "54a73e65e779893a797fff4b",
-	"platinum": 1337,
-	"quantity": 1,
-})
-	.then(response => {
-		console.log(response)
-		console.log("SUCCESS!")
-	})
-	.catch(console.error)
+				console.log(item.title)
+				console.log("Minimum:", min)
+				console.log("Average:", avg)
+				console.log("Median:", med)
+				console.log("Maximum:", max)
+				console.log("Minimum Plat per 10k Rep:", minPlatPer10kRep)
+				console.log("Average Plat per 10k Rep:", avgPlatPer10kRep)
+				console.log("Median Plat per 10k Rep:", medianPlatPer10kRep)
+				console.log("Maximum Plat per 10k Rep:", maxPlatPer10kRep)
+				console.log("\n")
+			})
+			.catch(console.error)
+	}, i * 500)	// Sleep 500ms between requests
+}
+const fs = require("fs")
+setTimeout(() => {
+	// Sort the items by median plat per 10k rep
+	parsedItems.sort((a, b) => b.medianPlatPer10kRep - a.medianPlatPer10kRep)
+	fs.writeFile(
+		"./red_veil_parsed.json",
+		parsedItems.map(item => JSON.stringify(item)).join('\n'),
+		{},
+		() => {}
+	);
+}, items.length * 500 + 1000)	// Wait for all requests to finish
+
